36462df96836c156cd5161a855f82e35ffdbb9ed
Oleksandr Sysenko <oleksandr_sysenko@epam.com>
2022-04-19 16:08:01 +0300

    add Article model

---
+++ NewsTime/NewsTime/Models/Article.swift
@@ -0,0 +1,36 @@
+//
+//  Article.swift
+//  NewsTime
+//
+//  Created by Oleksandr Sysenko on 19.04.2022.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    var id: Int
+    var url: String
+    var section: String
+    var byLine: String
+    var type: String
+    var title: String
+    var abstract: String
+    var publishedDate: String
+    var source: String
+    var keywords: [String]
+    var media: [Media]
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case url
+        case section
+        case byLine = "byline"
+        case type
+        case title
+        case abstract
+        case publishedDate = "published_date"
+        case source
+        case keywords = "des_facet"
+        case media
+    }
+}
